baf1d29ceab3390eb57c85789c48045e3c81d6fd
maxtang2013 <yllfever@gmail.com>
2017-02-28 16:54:59 +0800

    ---

---
+++ workrecords/upload.rb
@@ -0,0 +1,18 @@
+# filename: upload.rb
+#
+require 'selenium-webdriver'
+require 'rspec/expectations'
+include RSpec::Matchers
+
+def setup
+    @driver = Selenium::WebDriver.for :firefox
+end
+def teardown
+    @driver.quit
+end
+
+def run
+    setup
+    yield
+    teardown
+end
